2d3296eff94ceb96ddf518f114d47de892947d93
AKSHAT TANDON <akshat.tandon@stackguardian.io>
2023-08-30 16:58:13 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# transform-bulksg
+# StackGuardian Migrator
+
+Migrate workloads from other platforms to [StackGuardian Platform](https://app.stackguardian.io).
+
+## Supported platforms for migration
+
+- Terraform Cloud
+
+## Overview
+
+- Extract and transform the workloads from the target platform to StackGuardian Workflows.
+- Review the bulk workflow creation payload.
+- Run sg-cli with the bulk workflow creation payload.
+
+## Prerequisites
+
+- Terraform
+  - terraform login to ensure that Terraform can interact with your Terraform Cloud/Enterprise account.
+- [sg-cli](https://github.com/StackGuardian/sg-cli/tree/main/shell)
---
+++ transformer/tfc/main.tf
@@ -0,0 +1,109 @@
+# TODO: update all versions
+
+terraform {
+  required_version = "~> 1.2"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.1"
+    }
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "~> 0.45.0"
+    }
+  }
+}
+
+locals {
+  workflow_ids   = [for i, v in data.tfe_workspace_ids.all.ids : v]
+  workflow_names = [for i, v in data.tfe_workspace_ids.all.ids : i]
+  workflows = [for i, v in data.tfe_workspace_ids.all.ids : {
+    autodeploy = data.tfe_workspace.all[i].auto_apply
+    env_vars = [for i, v in data.tfe_variables.all[v].variables : {
+      hcl       = v.hcl
+      name      = v.category == "terraform" ? "TF_VAR_${v.name}" : v.name
+      sensitive = v.sensitive
+      value     = v.value
+    }]
+    labels            = data.tfe_workspace.all[i].tag_names
+    manage_state      = var.export_state
+    name              = data.tfe_workspace.all[i].name
+    terraform_version = data.tfe_workspace.all[i].terraform_version
+    vcs = {
+      # The "identifier" argument contains the acccount/organization and the respository names, separated by a slash
+      account = length(data.tfe_workspace.all[i].vcs_repo) > 0 ? split("/", data.tfe_workspace.all[i].vcs_repo[0].identifier)[1] : ""
+
+      # When the branch for the workflow is the repository's default branch, the value is empty so we use the value provided via the variable
+      branch = length(data.tfe_workspace.all[i].vcs_repo) > 0 ? data.tfe_workspace.all[i].vcs_repo[0].branch != "" ? data.tfe_workspace.all[i].vcs_repo[0].branch : var.vcs_default_branch : var.vcs_default_branch
+
+      namespace    = var.vcs_namespace
+      project_root = data.tfe_workspace.all[i].working_directory
+
+      # TFC/TFE does not return the VCS provider name so we use the value provided via the variable
+      provider = var.vcs_provider
+
+      # The "identifier" argument contains the acccount/organization and the respository names, separated by a slash
+      repository = length(data.tfe_workspace.all[i].vcs_repo) > 0 ? split("/", data.tfe_workspace.all[i].vcs_repo[0].identifier)[1] : ""
+    }
+  }]
+  data = jsonencode({
+    "workflows" : local.workflows
+  })
+}
+
+data "tfe_workspace_ids" "all" {
+  exclude_tags = var.tfc_workspace_exclude_tags
+  names        = var.tfc_workspace_names
+  organization = var.tfc_organization
+  tag_names    = var.tfc_workspace_include_tags
+}
+
+data "tfe_workspace" "all" {
+  for_each = toset(local.workflow_names)
+
+  name         = each.key
+  organization = var.tfc_organization
+}
+
+data "tfe_variables" "all" {
+  for_each = toset(local.workflow_ids)
+
+  workspace_id = each.key
+}
+
+resource "local_file" "data" {
+  content  = local.data
+  filename = "${path.module}/../../out/data.json"
+}
+
+resource "local_file" "generate_temp_tf_files" {
+  for_each = var.export_state ? toset(local.workflow_names) : []
+
+  content  = templatefile("${path.module}/workspace.tmpl", { tfc_organization = var.tfc_organization, workspace = each.key })
+  filename = "${path.module}/../../out/tf-files/${each.key}/main.tf"
+}
+
+resource "null_resource" "export_state_files" {
+  depends_on = [local_file.generate_temp_tf_files]
+  for_each   = var.export_state ? toset(local.workflow_names) : []
+
+  provisioner "local-exec" {
+    command     = "mkdir -p ../../state-files && rm -rf .terraform .terraform.lock.hcl terraform.tfstate terraform.tfstate.backup && terraform init -input=false && terraform state pull > ../../state-files/'${each.key}.tfstate'"
+    working_dir = "${path.module}/../../out/tf-files/${each.key}"
+  }
+}
+
+resource "null_resource" "delete_temp_tf_files" {
+  count      = var.export_state ? 1 : 0
+  depends_on = [null_resource.export_state_files]
+
+  provisioner "local-exec" {
+    command     = "rm -rf tf-files"
+    working_dir = "${path.module}/../../out/"
+  }
+}
