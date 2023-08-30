c97a8f76115cdabbd06f38c42e3816f0e149446f
rixhieloomis <richard.loomis@stackguardian.io>
2023-08-30 07:28:48 +0530

    Changes to push new revisions from the given state file

--- main.py
+++ main.py
@@ -1,16 +1,69 @@
 import json
 
-def edit_json(input_file, output_file):
-    # Read the data from the input JSON file
-    with open(input_file, 'r') as f:
-        data = json.load(f)
+def create_new_json(input_file1, input_file2):
+    try:
+        # Load data from the first input JSON file
+        with open(input_file1, 'r') as f1:
+            data1 = json.load(f1)        
+        
+        
 
-    # Perform edits on the data (example: adding a new key-value pair)
-    data['new_key'] = 'new_value'
+        #patch the data to the sg_payload
+        resources = (data1["resources"])
+        env = []
+        resource_names=[]
+        for i in resources:
+            if i["type"] == "tfe_variables":
+                instances = i["instances"]
+                for j in instances:
+                   if j["attributes"]["env"]:
+                    for k in j["attributes"]["env"]:
+                     if k["category"] == "env":
+                        env.append({k["name"] : k["value"]}) 
+                   if j["attributes"]["variables"]: 
+                    for k in j["attributes"]["variables"]:
+                     if k["category"] == "env":
+                        env.append({k["name"] : k["value"]})
+                
+            if i["type"] == "tfe_workspace":
+                workspace_names = i["instances"]
+                for j in workspace_names:
+                    workspace_name = j["index_key"]
+                    description = j["attributes"]["description"]
+                    tags = j["attributes"]["tag_names"]
+                    env = env
+                    resource_names.append({"ResourceName": workspace_name, "Description" : description, "Tags" : tags, "EnvironmentVariables" : env})
+                
+        
+            
+        for i in resource_names:
+            # Load data from the second input JSON file
+            with open(input_file2, 'r') as f2:
+                data2 = json.load(f2) 
+            
+            data2["ResourceName"] = i["ResourceName"]
+            data2["Description"] = i["Description"]
+            data2["Tags"] = i["Tags"]
+            data2["EnvironmentVariables"] = i["EnvironmentVariables"]
 
-    # Write the edited data to the output JSON file
-    with open(output_file, 'w') as f:
-        json.dump(data, f, indent=4)
+            with open(i["ResourceName"] + ".json", 'w') as out_f:
+                json.dump( data2, out_f, indent=4)
+            print(f"New JSON file {i['ResourceName']}.json created successfully.")
+        
+        # Write the data from the second input JSON file to the output JSON file
+        # with open(output_file, 'w') as out_f:
+        #     json.dump(data2, out_f, indent=4)
+        
+        
+    
+    except FileNotFoundError:
+        print("One or both input files not found.")
+    except json.JSONDecodeError:
+        print("Error decoding JSON data from the input file.")
 
-# Replace 'input.json' and 'output.json' with your file names
-edit_json('input.json', 'output.json')
+# Provide the paths of the input JSON files and the output JSON file
+input_json_file1 = 'example.json'
+input_json_file2 = 'sg_payload.json'
+
+
+create_new_json(input_json_file1,input_json_file2)
