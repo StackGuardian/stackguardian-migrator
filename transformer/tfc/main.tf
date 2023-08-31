c54d7ea23113a2ce80492dfa0266fe4fc8979acc
rixhieloomis <richard.loomis@stackguardian.io>
2023-08-31 10:52:08 +0530

    Absolute path for state file

--- transformer/tfc/main.tf
+++ transformer/tfc/main.tf
@@ -24,8 +24,8 @@ locals {
   workflow_names = [for i, v in data.tfe_workspace_ids.all.ids : i]
   workflows = [for i, v in data.tfe_workspace_ids.all.ids : {
      CLIConfiguration = {
-      "WorkflowGroup": "../../out/state-files/${data.tfe_workspace.all[i].name}.tfstate",
-      "TfStateFilePath": ""
+      "WorkflowGroup": "",
+      "TfStateFilePath": "${abspath(path.root)}../../out/state-files/${data.tfe_workspace.all[i].name}.tfstate"
     }
     ResourceName              = data.tfe_workspace.all[i].name
     wfgrpName = ""
