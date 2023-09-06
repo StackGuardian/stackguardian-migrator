8718be364f48df6c439ca1ff265398f748be52ae
rixhieloomis <richard.loomis@stackguardian.io>
2023-09-06 23:33:55 +0530

    Refactor

---
+++ transformer/exporter/main.tf
@@ -0,0 +1,20 @@
+# TODO: update all versions
+
+terraform {
+  required_version = "~> 1.2"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.1"
+    }
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "~> 0.45.0"
+    }
+  }
+}
