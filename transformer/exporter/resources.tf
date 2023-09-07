7163f8e1f248a57386730fe0db62c6bdd0ad4132
rixhieloomis <richard.loomis@stackguardian.io>
2023-09-07 14:15:46 +0530

    Exporth Path as var

--- transformer/exporter/resources.tf
+++ transformer/exporter/resources.tf
@@ -1,13 +1,13 @@
 resource "local_file" "data" {
   content  = local.data
-  filename = "${path.module}/../../export/sg-payload.json"
+  filename = "${path.module}/../../${var.exportPath}/sg-payload.json"
 }
 
 resource "local_file" "generateTempTfFiles" {
   for_each = var.stateExport ? toset(local.workflowNames) : []
 
   content  = templatefile("${path.module}/workspace.tmpl", { tfOrg = var.tfOrg, workspace = each.key })
-  filename = "${path.module}/../../export/tfDir/${each.key}/main.tf"
+  filename = "${path.module}/../../${var.exportPath}/tfDir/${each.key}/main.tf"
 }
 
 resource "null_resource" "exportStateFiles" {
@@ -16,7 +16,7 @@ resource "null_resource" "exportStateFiles" {
 
   provisioner "local-exec" {
     command     = "mkdir -p ../../states && rm -rf .terraform .terraform.lock.hcl terraform.tfstate terraform.tfstate.backup && terraform init -input=false && terraform state pull > ../../states/'${each.key}.tfstate'"
-    working_dir = "${path.module}/../../export/tfDir/${each.key}"
+    working_dir = "${path.module}/../../${var.exportPath}/tfDir/${each.key}"
   }
 }
 
@@ -26,6 +26,6 @@ resource "null_resource" "deleteTempTfFiles" {
 
   provisioner "local-exec" {
     command     = "rm -rf tfDir"
-    working_dir = "${path.module}/../../export/"
+    working_dir = "${path.module}/../../${var.exportPath}/"
   }
 }
